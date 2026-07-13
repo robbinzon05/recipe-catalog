5fb774bc9a1edddbbaa9973d46d33da33440bb4f
Mikhail <mihailohrimenko300@gmail.com>
2026-07-13 14:49:41 +1000

    Add recipe detail page, category filtering

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('recipes/', views.recipe_list, name='recipe_list'),
+    path('categories/<slug:slug>/', views.recipe_category, name='recipe_category'),
+    path('recipes/<slug:slug>/', views.recipe_detail, name='recipe_detail'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, render
 
 from .models import Category, Recipe
 
@@ -23,7 +23,39 @@ def recipe_list(request):
     page_obj = paginator.get_page(page_number)
 
     context = {
+        'categories': Category.objects.all(),
         'page_obj': page_obj,
         'recipes': page_obj.object_list,
     }
     return render(request, 'recipes/recipe_list.html', context)
+
+
+def recipe_category(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    recipes = Recipe.objects.select_related('category').filter(category=category)
+    paginator = Paginator(recipes, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'categories': Category.objects.all(),
+        'current_category': category,
+        'page_obj': page_obj,
+        'recipes': page_obj.object_list,
+    }
+    return render(request, 'recipes/recipe_list.html', context)
+
+
+def recipe_detail(request, slug):
+    recipe = get_object_or_404(
+        Recipe.objects.select_related('category').prefetch_related('comments__author'),
+        slug=slug,
+    )
+
+    context = {
+        'recipe': recipe,
+        'ingredients': recipe.ingredients.splitlines(),
+        'cooking_steps': recipe.cooking_steps.splitlines(),
+        'comments': recipe.comments.all(),
+    }
+    return render(request, 'recipes/recipe_detail.html', context)
