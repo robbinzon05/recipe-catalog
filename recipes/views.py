0f856ee08d1a31d3909e965684ebeb64e54fd886
Mikhail <mihailohrimenko300@gmail.com>
2026-07-13 14:18:26 +1000

    Add catalog home page, recipe list page

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
-# Create your views here.
+from .models import Category, Recipe
+
+
+def home(request):
+    categories = Category.objects.all()
+    featured_recipes = Recipe.objects.select_related('category')[:6]
+
+    context = {
+        'categories': categories,
+        'featured_recipes': featured_recipes,
+        'recipe_count': Recipe.objects.count(),
+    }
+    return render(request, 'recipes/home.html', context)
+
+
+def recipe_list(request):
+    recipes = Recipe.objects.select_related('category')
+    paginator = Paginator(recipes, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+        'recipes': page_obj.object_list,
+    }
+    return render(request, 'recipes/recipe_list.html', context)
