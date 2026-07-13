faebd274b788303067af1679c781ba10210d3499
Mikhail <mihailohrimenko300@gmail.com>
2026-07-13 12:48:30 +1000

    Configure Django admin for recipes

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Comment, Recipe
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'created_at')
+    search_fields = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+    readonly_fields = ('created_at',)
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+    fields = ('author', 'text', 'created_at')
+    readonly_fields = ('created_at',)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'created_at', 'updated_at')
+    list_filter = ('category', 'created_at')
+    search_fields = ('title', 'description', 'ingredients')
+    prepopulated_fields = {'slug': ('title',)}
+    readonly_fields = ('created_at', 'updated_at')
+    inlines = (CommentInline,)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'author', 'created_at')
+    list_filter = ('created_at', 'recipe__category')
+    search_fields = ('text', 'author__username', 'recipe__title')
+    readonly_fields = ('created_at', 'updated_at')
